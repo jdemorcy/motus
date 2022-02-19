85af1b13d78e2a91a6bdbc54a666f69c3229242b
djib <jdemorcy@gmail.com>
2022-02-20 00:40:05 +0100

    Stable version - Automatic grid generation and attributes assignement to the squares

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:motus/shared/constants.dart';
+import 'package:motus/tools/row_builder.dart';
 
 void main() {
   runApp(const MyApp());
@@ -32,188 +34,151 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
-  String letter1 = '';
-  String letter2 = '';
-  String letter3 = '';
-  String letter4 = '';
-  String letter5 = '';
+  void evaluateLetter({String? result, String? ref}) {
 
-  Map<String,String> mapLetters = {
-    'letter_1': "L",
-    'letter_2': "A",
-    'letter_3': "P",
-    'letter_4': "I",
-    'letter_5': "N",
-  };
-
-  Map<String,BoxDecoration> mapLettersDecoration = {
-    'letter_1': letterDecorationNotFound,
-    'letter_2': letterDecorationNotFound,
-    'letter_3': letterDecorationNotFound,
-    'letter_4': letterDecorationNotFound,
-    'letter_5': letterDecorationNotFound,
-  };
-
-
-  void evaluateLetter({String? res, String? target}) {
     setState(() {
-      if(res == 'found') mapLettersDecoration[target!] = letterDecorationFoundRightPosition;
-      if(res == 'notFound') mapLettersDecoration[target!] = letterDecorationNotFound;
-      if(res == 'wrongPosition') mapLettersDecoration[target!] = letterDecorationFoundWrongPosition;
+      if (result == 'found') {
+        mapSquareDecoration[ref!] = letterDecorationFoundRightPosition;
+      }
+      if (result == 'notFound') {
+        mapSquareDecoration[ref!] = letterDecorationNotFound;
+      }
+      if (result == 'wrongPosition') {
+        mapSquareDecoration[ref!] = letterDecorationFoundWrongPosition;
+      }
+
     });
+
+  }
+
+  List<Widget> printRow({int? rowNum}) {
+    var lst = <Widget>[];
+    var ref;
+
+    for(var i=1; i<=5; i++) {
+      ref = rowNum.toString() + i.toString();
+      lst.add(RowBuilder(ref: ref));
+    }
+
+    return lst;
   }
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
         toolbarHeight: 80.0,
         title: Text(
-            widget.title,
+          widget.title,
           style: const TextStyle(
             fontSize: 50.0,
           ),
         ),
         elevation: 0.0,
       ),
-      body: Column(
-        children: <Widget>[
-          Container(
-          padding: const EdgeInsets.all(20.0),
-          color: Colors.black,
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              // Letter #1
-              Container(
-              width: 60.0,
-              height: 60.0,
-              padding: EdgeInsets.fromLTRB(10.0, 5.0, 10.0, 10.0),
-              decoration: mapLettersDecoration['letter_1'],
-              child: Text(
-                mapLetters['letter_1']!,
-                textAlign: TextAlign.center,
-                style: const TextStyle(
-                  fontSize: 40.0,
-                  color: Colors.white,
-                ),
-              ),
+      body:
+      Container(
+        padding: const EdgeInsets.all(20.0),
+        color: Colors.grey[800],
+        child: Column(
+          children: [
+            // Grid
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: printRow(rowNum: 1),
             ),
-              SizedBox(width: 10.0),
 
-              // Letter #2
-              Container(
-                width: 60.0,
-                height: 60.0,
-                padding: EdgeInsets.fromLTRB(10.0, 5.0, 10.0, 10.0),
-                decoration: mapLettersDecoration['letter_2'],
-                child: Text(
-                  mapLetters['letter_2']!,
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(
-                    fontSize: 40.0,
-                    color: Colors.white,
+            SizedBox(height: 20.0),
+
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: printRow(rowNum: 2),
+            ),
+
+            SizedBox(height: 20.0),
+
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: printRow(rowNum: 3),
+            ),
+
+            SizedBox(height: 20.0),
+
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: printRow(rowNum: 4),
+            ),
+
+            SizedBox(height: 20.0),
+
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: printRow(rowNum: 5),
+            ),
+
+            SizedBox(height: 20.0),
+
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: printRow(rowNum: 6),
+            ),
+
+            SizedBox(height: 20.0),
+
+            // Buttons
+            Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  ElevatedButton(
+                    onPressed: () =>
+                       evaluateLetter(result: 'notFound', ref:'25'),
+                    style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(
+                            Colors.blueGrey)
+                    ),
+                    child: const Text(
+                      'Not Found',
+                    ),
                   ),
-                ),
-              ),
-              SizedBox(width: 10.0),
+                  SizedBox(width: 20.0),
 
-              // Letter #3
-              Container(
-                width: 60.0,
-                height: 60.0,
-                padding: EdgeInsets.fromLTRB(10.0, 5.0, 10.0, 10.0),
-                decoration: mapLettersDecoration['letter_3'],
-                child: Text(
-                  mapLetters['letter_3']!,
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(
-                    fontSize: 40.0,
-                    color: Colors.white,
+                  ElevatedButton(
+                    onPressed: () =>
+                        evaluateLetter(result: 'wrongPosition', ref:'25'),
+                    style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(
+                            Colors.blueGrey)
+                    ),
+                    child: const Text(
+                      'Wrong',
+                    ),
                   ),
-                ),
-              ),
-              SizedBox(width: 10.0),
+                  SizedBox(width: 20.0),
 
-              // Letter #4
-              Container(
-                width: 60.0,
-                height: 60.0,
-                padding: EdgeInsets.fromLTRB(10.0, 5.0, 10.0, 10.0),
-                decoration: mapLettersDecoration['letter_4'],
-                child: Text(
-                  mapLetters['letter_4']!,
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(
-                    fontSize: 40.0,
-                    color: Colors.white,
+                  ElevatedButton(
+                    onPressed: () =>
+                        evaluateLetter(result:'found', ref:'12'),
+                    style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(
+                            Colors.blueGrey)
+                    ),
+                    child: const Text(
+                      'Found',
+                    ),
                   ),
-                ),
-              ),
-              SizedBox(width: 10.0),
+                ]
+            ),
+          ],
 
-              // Letter #5
-              Container(
-                width: 60.0,
-                height: 60.0,
-                padding: EdgeInsets.fromLTRB(10.0, 5.0, 10.0, 10.0),
-                decoration: mapLettersDecoration['letter_5'],
-                child: Text(
-                  mapLetters['letter_5']!,
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(
-                    fontSize: 40.0,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-
-            ],
-          ),
         ),
-        SizedBox(height: 20.0),
-
-          // Action buttons
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              ElevatedButton(
-                onPressed: () => evaluateLetter(res: 'notFound', target: 'letter_2'),
-                style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all(Colors.blueGrey)
-                ),
-                child: const Text(
-                  'Not Found',
-                ),
-              ),
-              SizedBox(width: 20.0),
-
-              ElevatedButton(
-                onPressed: () => evaluateLetter(res: 'wrongPosition', target: 'letter_2'),
-                style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all(Colors.blueGrey)
-                ),
-                child: const Text(
-                  'Wrong',
-                ),
-              ),
-              SizedBox(width: 20.0),
-
-              ElevatedButton(
-                onPressed: () => evaluateLetter(res: 'found', target: 'letter_2'),
-                style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all(Colors.blueGrey)
-                ),
-                child: const Text(
-                  'Found',
-                ),
-              ),
-          ]
-          ),
-        ]
       ),
+
     );
   }
 }
