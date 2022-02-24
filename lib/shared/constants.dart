d5772b6487ba392bed093a0d491f88531a5ef5bf
djib <jdemorcy@gmail.com>
2022-02-24 19:18:18 +0100

    Stable version - Form field added with validation

--- lib/shared/constants.dart
+++ lib/shared/constants.dart
@@ -118,4 +118,16 @@ Map<String,BoxDecoration> mapSquareDecoration = {
 
 };
 
+// Form field decoration
+const textInputDecoration = InputDecoration(
+  fillColor: Colors.white,
+  filled: true,
+  enabledBorder: OutlineInputBorder(
+    borderSide: BorderSide(color: Colors.blueGrey, width: 2.0),
+  ),
+  focusedBorder: OutlineInputBorder(
+    borderSide: BorderSide(color: Colors.blue, width: 2.0),
+  ),
+);
+
 
